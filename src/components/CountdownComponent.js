ea19bd8234e36c04eb9733324bbdb32e41edeeb8
Ibán López <ibanlopezd@gmail.com>
2020-08-11 19:00:21 +0200

    Ready to deploy

--- src/components/CountdownComponent.js
+++ src/components/CountdownComponent.js
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { Paper, Grid, Typography, LinearProgress } from '@material-ui/core';
+import useSound from 'use-sound';
+import alert from '../alert.mp3';
 
 export default function Countdown({ timer, countdown, breaker, play, setCountdown }) {
 	const [ playBreaker, setPlayBreaker ] = useState(false);
+	const [ showTimer, setShowTimer ] = useState(false);
+	const [ playAlert ] = useSound(alert);
 
 	useEffect(
 		() => {
@@ -21,39 +25,58 @@ export default function Countdown({ timer, countdown, breaker, play, setCountdow
 			if (playBreaker && countdown < 0) {
 				setCountdown(timer * 60);
 				setPlayBreaker(false);
+				setShowTimer(true);
+				setTimeout(() => {
+					setShowTimer(false);
+				}, 2000);
+				playAlert();
 			} else if (!playBreaker && countdown < 0) {
 				setCountdown(breaker * 60);
 				setPlayBreaker(true);
+				setShowTimer(true);
+				setTimeout(() => {
+					setShowTimer(false);
+				}, 2000);
+				playAlert();
 			}
 		},
-		[ countdown, setCountdown, playBreaker, breaker, timer ]
+		[ countdown, setCountdown, playBreaker, breaker, timer, playAlert ]
 	);
 
 	const convert = (time) => {
 		let minutes = Math.floor(time / 60);
 		let seconds = time - minutes * 60;
-
 		function str_pad_left(string, pad, length) {
 			return (new Array(length + 1).join(pad) + string).slice(-length);
 		}
-
 		let finalTime = str_pad_left(minutes, '0', 2) + ':' + str_pad_left(seconds, '0', 2);
-
 		return finalTime;
 	};
 
 	const normalise = (countdown, total) => 100 * countdown / (total * 60);
+	const text = !playBreaker ? 'Session Started' : 'Break Started';
 
 	return (
 		<Grid container justify='center' style={{ marginTop: '3vh' }}>
-			<Paper id='time-left' elevation={3} style={{ width: '100%', textAlign: 'center' }}>
-				<Typography variant='h1'>{convert(countdown)}</Typography>
+			<Paper elevation={3} style={{ width: '100%', textAlign: 'center' }}>
+				<Typography id='time-left' variant='h1'>
+					{convert(countdown)}
+				</Typography>
+
+				{showTimer && (
+					<Typography id='timer-label' variant='h4'>
+						{text}
+					</Typography>
+				)}
+
 				{!playBreaker ? (
 					<LinearProgress variant='determinate' value={normalise(countdown, timer)} />
 				) : (
 					<LinearProgress variant='determinate' value={normalise(countdown, breaker)} />
 				)}
 			</Paper>
+			{/*React @useSound hook used instead of HTML5 audio element*/}
+			<p id='beep' />
 		</Grid>
 	);
 }
